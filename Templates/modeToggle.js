ba88c6653f866ece906266083c061690b4745ddd
Nemanja Stefanovic <museg@Nemanjas-MacBook-Pro.local>
2022-04-20 17:00:39 +0200

    odradjen template za toggle

--- Templates/modeToggle.js
+++ Templates/modeToggle.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('.toggle_btn')
 const buttonBox = document.querySelector('.toggle_box')
 
+const body = document.querySelector('body')
+
 button.style.left = 0
 let btnPossition = 0
 
@@ -8,16 +10,38 @@ let btnPossition = 0
 const changeMode = () => {
     switch(btnPossition){
         case 0:
+            //Make animation
             button.style.removeProperty('left')
-            button.style.rigt = 0
+            button.style.right = 0
+            
+            //Switch mode to light
+            switchToLight()
+            
             btnPossition = 1
             break
             case 1:
+                //Make animation
                 button.style.removeProperty('right')
                 button.style.left = 0
+                
+                //Switch mode to dark
+                switchToDark()
+                
                 btnPossition = 0
-                break 
-            }
+                break
     }
-        
+}
+
+const switchToLight = () => {
+    buttonBox.style.background = "#f7f7f7"
+    button.style.background = "#1d1d1d"
+    body.style.background = "#c9c9c9"
+}
+const switchToDark = () => {
+    buttonBox.style.background = "#1d1d1d"
+    button.style.background = "#f7f7f7" 
+    body.style.background = "#292828"
+}
+
 buttonBox.addEventListener("click", changeMode)
+
